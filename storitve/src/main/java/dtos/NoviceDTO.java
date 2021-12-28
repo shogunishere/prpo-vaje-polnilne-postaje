62d0dc8444ba7fddd7dba3c21dfce30edcb16e66
shogunishere <bc9389@student.uni-lj.si>
2021-12-28 20:58:01 +0100

    dodan DTO

---
+++ storitve/src/main/java/dtos/NoviceDTO.java
@@ -0,0 +1,31 @@
+package dtos;
+
+public class NoviceDTO {
+    private String naslov;
+    private String url;
+    private String source;
+
+    public String getNaslov() {
+        return naslov;
+    }
+
+    public void setNaslov(String naslov) {
+        this.naslov = naslov;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+}
---
+++ storitve/src/main/java/zrna/UpravljanjePolnilnicZrno.java
@@ -0,0 +1,12 @@
+package zrna;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class UpravljanjePolnilnicZrno {
+    // poslovne metode:
+    // 1. stevilo prostih prikljuckov (izracun)
+    // 2. rezervacija najema
+    // 3.
+
+}
